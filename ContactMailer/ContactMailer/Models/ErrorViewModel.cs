757b02ca43b68f9ae07ed97d1724dfbe211ccd52
Nikos Kekes <nickkekes@gmail.com>
2020-03-06 20:11:17 +0200

    VS2019 Project create

---
+++ ContactMailer/ContactMailer/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContactMailer.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
