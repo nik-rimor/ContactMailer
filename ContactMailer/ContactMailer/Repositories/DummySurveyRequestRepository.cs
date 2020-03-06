8e8f749522cd2f9120fb766dcbba234c3c6be03e
Nikos Kekes <nickkekes@gmail.com>
2020-03-06 21:41:23 +0200

    setup dummy repository

---
+++ ContactMailer/ContactMailer/Repositories/DummySurveyRequestRepository.cs
@@ -0,0 +1,91 @@
+﻿using ContactMailer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactMailer.Repositories
+{
+    public class DummySurveyRequestRepository : ISurveyRequestRepository
+    {
+        private Dictionary<Guid, SurveyRequest> _requersts = new Dictionary<Guid, SurveyRequest>()
+        {
+            [Guid.Parse("24a29891-0f4b-490c-94ab-340229a836e9")] = new SurveyRequest
+            {
+                Id = Guid.Parse("24a29891-0f4b-490c-94ab-340229a836e9"),
+                EmailAddress = "someone@somewhere.net",
+                FullName = "John Doe",
+                SubmissionDate = null,
+                Status = SurveyRequestStatus.Pending,
+            },
+        };
+
+        public SurveyRequest Get(Guid id)
+        {
+            _requersts.TryGetValue(id, out SurveyRequest req);
+            return req;
+        }
+
+        public SurveyRequest GetByEmailAddress(string emailAddress)
+        {
+            foreach(KeyValuePair<Guid, SurveyRequest> item in _requersts)
+            {
+                if (item.Value.EmailAddress == emailAddress)
+                    return item.Value;
+            }
+            return null;
+        }
+
+        public IList<SurveyRequest> GetByStatus(SurveyRequestStatus status)
+        {
+            IList<SurveyRequest> requestsList = new List<SurveyRequest>();
+            foreach(KeyValuePair<Guid, SurveyRequest> item in _requersts)
+            {
+                if (item.Value.Status == status)
+                    requestsList.Add(item.Value);                
+            }
+            return requestsList;
+
+        }
+
+        public bool Update(SurveyRequest req)
+        {
+            try
+            {
+                _requersts[req.Id] = req;
+                return true;
+            }
+            catch {
+                return false;
+            }
+
+        }
+
+        public bool Insert(SurveyRequest req)
+        {
+            try
+            {
+                _requersts.Add(req.Id, req);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }            
+        }
+
+        public bool Delete(SurveyRequest req)
+        {
+            try
+            {
+                _requersts.Remove(req.Id);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ ContactMailer/ContactMailer/Repositories/ISurveyRequestRepository.cs
@@ -0,0 +1,23 @@
+﻿using ContactMailer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContactMailer.Repositories
+{
+    interface ISurveyRequestRepository
+    {
+        SurveyRequest Get(Guid id);
+
+        SurveyRequest GetByEmailAddress(string emailAddress);
+
+        IList<SurveyRequest> GetByStatus(SurveyRequestStatus status);
+
+        bool Update(SurveyRequest req);
+
+        bool Insert(SurveyRequest req);
+
+        bool Delete(SurveyRequest req);
+    }
+}
